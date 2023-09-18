c55cecde11b907c51e34fc131a49d0e0c657160f
Victor <victor.henrique5800@gmail.com>
2023-09-18 14:57:46 -0300

    Implementando base

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import part3 as part3
 
 
 spark = SparkSession.builder \
-    .appName("HelloLines") \
+    .appName("tp1_victor_e_arthur") \
     .getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("WARN")
@@ -13,10 +13,10 @@ sc.setLogLevel("WARN")
 print("==================== Iniciando script ====================\n")
 
 dataset_path = "hdfs://localhost:9000/datasets/spotify/"
+playlists_df = spark.read.json(dataset_path + 'playlist.json')
 tracks_df = spark.read.json(dataset_path + 'tracks.json')
-playlists_df = spark.read.json(dataset_path + 'playlists.json')
 
-part2.run(sc, dataset_path)
+part2.run(sc, tracks_df, playlists_df)
 
 print("\n==================== Fim do script ====================\n")
 sc.stop()
